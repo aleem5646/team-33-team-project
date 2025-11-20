a84b35f0e4fc9caf0a071fd62a9620fcb49f63dd
ana <ana24.maria05@gmail.com>
2025-11-20 09:36:38 +0000

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,13 +8,11 @@ class ProductController extends Controller
 {
     public function show($id)
     {
-        // For now, just return the static product view
         return view('product-detail');
     }
 
     public function addToCart(Request $request)
     {
-        // Later you can add real cart logic here
         return back()->with('status', 'Added to cart!');
     }
 }
