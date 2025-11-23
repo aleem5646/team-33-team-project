5a2757730e926a54578791c6a5b1c61baa78ea44
Nabil Yanna Safianou <nabilsafianou@gmail.com>
2025-11-23 20:50:21 +0000

    Restoring lost changes

--- resources/views/layouts/customer-layout.blade.php
+++ resources/views/layouts/customer-layout.blade.php
@@ -4,22 +4,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     {{--include main source files for css and js--}}
-    @vite(["resources/css/app.css", "resources/js/app.js"]);
+    @vite(["resources/css/app.css", "resources/js/app.js"])
 
     {{--page title to display--}}
-    <title>@yield("title")</title>
+    <title>@yield("title", "Solara")</title>
 </head>
 <body>
 
-    @include("navbar")
+    <x-nav-bar /> 
 
-    <div>
-        @section("page")
-        @show
-    </div>
+    <main class="mx-auto">
+        @yield("content")
+    </main>
 
-    @include()
-    @include("footer")
+    <x-footer />
 
 </body>
 </html>
