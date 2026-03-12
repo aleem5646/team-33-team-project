d7efcc93b8811576050540020f1d99f541fba8d1
Nabil Safianou <nabilsafianou@gmail.com>
2026-03-12 17:50:55 +0000

    Added file update

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -4,19 +4,17 @@ const openState = document.getElementById("open-icon");
 const closedSate = document.getElementById("close-icon");
 
 toggleButton.addEventListener("click", () => {
-
     //  Check if the dropdown is hidden and if so display it
     if (nav.classList.contains("hidden")) {
         nav.classList.replace("hidden", "flex");
         openState.classList.add("hidden");
         closedSate.classList.replace("hidden", "flex");
     }
-    
+
     // If already displayed then hide it
     else {
         nav.classList.replace("flex", "hidden");
         openState.classList.replace("hidden", "flex");
         closedSate.classList.replace("flex", "hidden");
     }
-
 });
