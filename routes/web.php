5b8d4f6644802a3f56a6b7f5ef0f12132d1ce350
Zaak32 <zakhandley03@gmail.com>
2026-03-21 15:25:52 +0000

    Routing Change

--- routes/web.php
+++ routes/web.php
@@ -91,7 +91,7 @@ Route::get('/products/{id}', [ProductController::class, 'show'])->name('products
 
 Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard')->middleware(['auth', 'verified']);
 Route::get('admin/customers', [AdminController::class, 'customers'])->name('admin.customers.index')->middleware(['auth', 'verified']);
-Route::get('admin/customers/{customer}', [AdminController::class, 'showCustomer'])->name('admin.customers.show')->middleware(['auth', 'verified']);
+Route::get('admin/customers/{id}', [CustomerController::class, 'show'])->name('admin.customers.show')->middleware(['auth', 'verified']);
 # Add any routes that a logged in user can access in 
 # this route group
 Route::group(['middleware'=>['auth','verified']], function (){
